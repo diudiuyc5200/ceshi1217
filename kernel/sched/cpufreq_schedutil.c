e38b50be673e9200c760a3f31527fd80f6467db7
diudiuyc5200 <17766728994@163.com>
2025-12-22 08:10:01 +0800

    Update cpufreq_schedutil.c

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1100,7 +1100,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 	tunables->down_rate_limit_us =
 				cpufreq_policy_transition_delay_us(policy);
 	tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
-	tunables->hispeed_freq = 0;
+	tunables->hispeed_freq = 1708800;
 
 	switch (policy->cpu) {
 	default:
