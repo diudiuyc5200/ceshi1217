fb7e64bc08f7f7d2439f5b756f9721d67e9bf296
diudiuyc5200 <17766728994@163.com>
2025-12-23 10:28:37 +0800

    Update decompressor.c

--- fs/erofs/decompressor.c
+++ fs/erofs/decompressor.c
@@ -229,7 +229,10 @@ static int z_erofs_lz4_decompress(struct z_erofs_decompress_req *rq, u8 *dst)
 	/* legacy format could compress extra data in a pcluster. */
 	if (rq->partial_decoding || !support_0padding)
 		ret = LZ4_decompress_safe_partial(src + inputmargin, out,
-				rq->inputsize, rq->outputsize, rq->inplace_io);
+				rq->inputsize, rq->outputsize, rq->outputsize);
+	else
+		ret = LZ4_decompress_safe(src + inputmargin, out,
+					  rq->inputsize, rq->outputsize);
 	else
 		ret = LZ4_decompress_safe(src + inputmargin, out,
 					  rq->inputsize, rq->outputsize, rq->inplace_io);
