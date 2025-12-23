ccab061ab41c5945f13ca91293b935ad414e000e
diudiuyc5200 <17766728994@163.com>
2025-12-23 19:22:39 +0800

    Remove __netif_receive_skb_list function
    Removed the __netif_receive_skb_list function, which handled the reception of a list of socket buffers.

--- net/core/dev.c
+++ net/core/dev.c
@@ -4631,46 +4631,6 @@ static int __netif_receive_skb(struct sk_buff *skb)
 	return ret;
 }
 
-static void __netif_receive_skb_list(struct list_head *head)
-{
-	unsigned long noreclaim_flag = 0;
-	struct sk_buff *skb, *next;
-	bool pfmemalloc = false; /* Is current sublist PF_MEMALLOC? */
-
-	list_for_each_entry_safe(skb, next, head, list) {
-		if ((sk_memalloc_socks() && skb_pfmemalloc(skb)) != pfmemalloc) {
-			struct list_head sublist;
-
-			/* Handle the previous sublist */
-			list_cut_before(&sublist, head, &skb->list);
-			if (!list_empty(&sublist)) {
-				struct sk_buff *nskb, *nnext;
-				list_for_each_entry_safe(nskb, nnext, &sublist, list) {
-					list_del(&nskb->list);
-					__netif_receive_skb_core(nskb, pfmemalloc);
-				}
-			}
-			pfmemalloc = !pfmemalloc;
-			/* See comments in __netif_receive_skb */
-			if (pfmemalloc)
-				noreclaim_flag = memalloc_noreclaim_save();
-			else
-				memalloc_noreclaim_restore(noreclaim_flag);
-		}
-	}
-	/* Handle the remaining sublist */
-	if (!list_empty(head)) {
-		struct sk_buff *nskb, *nnext;
-		list_for_each_entry_safe(nskb, nnext, head, list) {
-			list_del(&nskb->list);
-			__netif_receive_skb_core(nskb, pfmemalloc);
-		}
-	}
-	/* Restore pflags */
-	if (pfmemalloc)
-		memalloc_noreclaim_restore(noreclaim_flag);
-}
-
 static int generic_xdp_install(struct net_device *dev, struct netdev_bpf *xdp)
 {
 	struct bpf_prog *old = rtnl_dereference(dev->xdp_prog);
